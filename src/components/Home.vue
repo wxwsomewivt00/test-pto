61c0a1fd4eed34ec972b653640b32b994a6552d8
wxwsomewivt00 <50981896+wxwsomewivt00@users.noreply.github.com>
2019-05-24 14:59:12 +0700

    Add files via upload

---
+++ src/components/Home.vue
@@ -0,0 +1,50 @@
+<!-- /components/Home.vue -->
+<template>
+  
+  <!-- Container (TOUR Section) -->
+<div id="up" class="bg-1">
+  <div class="container">
+    <h3 class="text-center">ทดสอบ</h3>
+    <p class="text-center">ทดสอบการเชื่อม API เเละแสดงผล.<br> โดยใช้ API จาก <strong>"https://jsonplaceholder.typicode.com/albums/1/photos"</strong></p>
+    
+    
+    <div class="row text-center">
+      <div class="col-sm-4"  v-for="item in test"> 
+        <div class="thumbnail">
+          <img :src="item.url" :alt="item.url">
+          <p><strong>ชื่อ :</strong>{{ item.title }}</p>
+          <button class="btn" data-toggle="modal" >{{ item.id }}</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+</div>
+
+</template>
+
+<script>
+const API_URL = 'https://jsonplaceholder.typicode.com/albums/1/photos'
+export default {
+  name: 'Home',
+  data: function () {
+    return {
+      test: [] 
+    }
+  },
+  methods: {
+    loadDate: function () {
+      this.$http.get(API_URL) //เรียกใช้ ้kttp ที่ API_URL
+        .then(response => {
+          // console.log(response)
+          this.test = response.body //เก็บไว้ในตัวแปร teest
+        }, () => {
+          this.test = []
+        })
+    }
+  },
+  created: function () {
+    this.loadDate()
+  }
+}
+</script>
---
+++ src/components/Test.vue
@@ -0,0 +1,46 @@
+<!-- src/components/Home.vue -->
+<template>
+  <div>
+    <h1>afsdfasdfasdf</h1>
+    <button type="button" class="btn btn-default btn-lg" @click="goAdd">
+      <span class="glyphicon glyphicon-plus" aria-hidden="true" ></span> Add
+    </button>
+    <br />
+    <br />
+    <div class="col-sm-6 col-md-4">
+      <div class="thumbnail">
+        <img src="http://placehold.it/600/92c952" alt="accusamus beatae ad facilis cum similique qui sunt">
+        <div class="caption">
+          <h3>accusamus beatae ad facilis cum similique qui sunt</h3>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-6 col-md-4">
+      <div class="thumbnail">
+        <img src="http://placehold.it/600/92c952" alt="accusamus beatae ad facilis cum similique qui sunt">
+        <div class="caption">
+          <h3>accusamus beatae ad facilis cum similique qui sunt</h3>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-6 col-md-4">
+      <div class="thumbnail">
+        <img src="http://placehold.it/600/92c952" alt="accusamus beatae ad facilis cum similique qui sunt">
+        <div class="caption">
+          <h3>accusamus beatae ad facilis cum similique qui sunt</h3>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Test',
+  methods: {
+    goAdd: function () {
+      this.$router.push('/add')
+    }
+  }
+}
+</script>
